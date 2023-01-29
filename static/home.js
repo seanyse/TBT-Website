9e2234da7c2aa43af1ba9aa3c115a71fdf28d30f
seanyse <seanyan0915@gmail.com>
2023-01-29 17:13:44 -0500

    mobile

--- static/home.js
+++ static/home.js
@@ -1,3 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
